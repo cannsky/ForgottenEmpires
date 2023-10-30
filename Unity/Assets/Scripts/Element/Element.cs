d6aac3daec79eb2b018bf715f214720c2f705490
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-10-30 23:36:02 +0300

    add: health & totalhealth to all elements

--- Unity/Assets/Scripts/Element/Element.cs
+++ Unity/Assets/Scripts/Element/Element.cs
@@ -4,6 +4,9 @@ namespace ForgottenEmpires.Elements
 {
     public abstract class Element : MonoBehaviour
     {
+        //These are network variables, needs to be updated continously and shown to the other players.
+        public float health, totalHealth;
 
+        public abstract void TakeDamage(float damage);
     }
 }
