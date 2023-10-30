dc68ec52586491d9ca6e5d9b157fdaa0cc25f763
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-10-30 23:35:29 +0300

    add: player attack & player stats to player worker

--- Unity/Assets/Scripts/Element/Player/Worker/PlayerWorker.cs
+++ Unity/Assets/Scripts/Element/Player/Worker/PlayerWorker.cs
@@ -4,14 +4,18 @@
     {
         public Player player;
 
+        public PlayerAttack playerAttack;
         public PlayerInput playerInput;
         public PlayerMovement playerMovement;
+        public PlayerStats playerStats;
 
         public PlayerWorker(Player player)
         {
             this.player = player;
+            playerAttack = new PlayerAttack(this);
             playerInput = new PlayerInput(this);
             playerMovement = new PlayerMovement(this);
+            playerStats = new PlayerStats(this);
         }
     }
 }
