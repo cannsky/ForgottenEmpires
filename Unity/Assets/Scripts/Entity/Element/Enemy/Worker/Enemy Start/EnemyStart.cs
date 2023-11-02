97a7550393102d0a3944a6012bfad07dfb2dc1f3
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-03 02:18:55 +0300

    add: enemy start & enemy update

---
+++ Unity/Assets/Scripts/Entity/Element/Enemy/Worker/Enemy Start/EnemyStart.cs
@@ -0,0 +1,25 @@
+﻿namespace ForgottenEmpires.Entity.Elements.Enemies.Worker
+{
+    public class EnemyStart
+    {
+        private EnemyWorker enemyWorker;
+
+        public EnemyStart(EnemyWorker enemyWorker) => this.enemyWorker = enemyWorker;
+
+        public void OnStart()
+        {
+            if (enemyWorker.enemy.isClient) OnClientStart();
+            if (enemyWorker.enemy.isServer) OnServerStart();
+        }
+
+        public void OnClientStart()
+        {
+
+        }
+
+        public void OnServerStart()
+        {
+            enemyWorker.enemyBehaviour.OnStart();
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Entity/Element/Enemy/Worker/Enemy Update/EnemyUpdate.cs
@@ -0,0 +1,25 @@
+﻿namespace ForgottenEmpires.Entity.Elements.Enemies.Worker
+{
+    public class EnemyUpdate
+    {
+        private EnemyWorker enemyWorker;
+
+        public EnemyUpdate(EnemyWorker enemyWorker) => this.enemyWorker = enemyWorker;
+
+        public void OnUpdate()
+        {
+            if (enemyWorker.enemy.isClient) OnClientUpdate();
+            if (enemyWorker.enemy.isServer) OnServerUpdate();
+        }
+
+        public void OnClientUpdate()
+        {
+
+        }
+
+        public void OnServerUpdate()
+        {
+            enemyWorker.enemyBehaviour.OnUpdate();
+        }
+    }
+}
