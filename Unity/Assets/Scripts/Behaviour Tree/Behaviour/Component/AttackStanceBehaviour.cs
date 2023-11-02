ac5653b4193b46b8ffe4d6c8af922f4c995a4b9f
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-03 02:19:12 +0300

    add: attack stance behaviour & enemy attack stance behaviour

---
+++ Unity/Assets/Scripts/Behaviour Tree/Behaviour/Component/AttackStanceBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using ForgottenEmpires.Types;
+
+namespace ForgottenEmpires.BehaviourTrees
+{
+    public class AttackStanceBehaviour : Behaviour
+    {
+        public override bool GetPredicate() => true;
+
+        public override void HandleBehaviour() => behaviourNode.behaviourTree.element.SetAnimation(AnimationType.Idle, true);
+    }
+}
---
+++ Unity/Assets/Scripts/Entity/Element/Enemy/Worker/Enemy Behaviour/Component/EnemyAttackStanceBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using ForgottenEmpires.BehaviourTrees;
+using ForgottenEmpires.Checkers;
+using ForgottenEmpires.Managers.Server;
+
+namespace ForgottenEmpires.Entity.Elements.Enemies.Worker
+{
+    public class EnemyAttackStanceBehaviour : RunBehaviour
+    {
+        private EnemyBehaviour enemyBehaviour;
+
+        public SingleRangeChecker singleRangeChecker;
+
+        public EnemyAttackStanceBehaviour(EnemyBehaviour enemyBehaviour)
+        {
+            this.enemyBehaviour = enemyBehaviour;
+            singleRangeChecker = new SingleRangeChecker(enemyBehaviour.enemyWorker.enemy, 2f,
+                ServerManager.Instance.serverManagerWorker.serverPlayerWorker.players);
+        }
+
+        public override bool GetPredicate() => singleRangeChecker.Check();
+    }
+}
