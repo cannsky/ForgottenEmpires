34b8a3ca43654f192c548e86faf8be553399f6df
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-02 22:54:21 +0300

    add: player rotation input

---
+++ Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Input/Player Rotation Input/PlayerRotationInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace ForgottenEmpires.Entity.Elements.PlayerWorkers
+{
+    public class PlayerRotationInput
+    {
+        private PlayerInput playerInput;
+
+        private Transform cameraTransform;
+
+        private Vector3 rotationDirection;
+
+        public PlayerRotationInput(PlayerInput playerInput)
+        {
+            this.playerInput = playerInput;
+            cameraTransform = Camera.main.transform;
+        }
+
+        public void OnUpdate() => SendRotationInput();
+
+        public void SendRotationInput()
+        {
+            rotationDirection = Vector3.zero;
+            rotationDirection = cameraTransform.forward * playerInput.movementInput.y;
+            rotationDirection += cameraTransform.right * playerInput.movementInput.x;
+            rotationDirection.Normalize();
+            rotationDirection.y = 0;
+            playerInput.playerWorker.player.PlayerRotationRequest(rotationDirection);
+        }
+    }
+}
