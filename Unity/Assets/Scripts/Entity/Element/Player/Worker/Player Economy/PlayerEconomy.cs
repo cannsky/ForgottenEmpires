80711516821ea279ff1eb514a20ebc5f485a9a48
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-03 20:49:06 +0300

    add: player economy

---
+++ Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Economy/PlayerEconomy.cs
@@ -0,0 +1,17 @@
+﻿namespace ForgottenEmpires.Entity.Elements.PlayerWorkers
+{
+    public class PlayerEconomy
+    {
+        private PlayerWorker playerWorker;
+
+        private float coins;
+
+        public PlayerEconomy(PlayerWorker playerWorker) => this.playerWorker = playerWorker;
+
+        public void AddCoins(float coins) => this.coins += coins;
+
+        public void DecreaseCoins(float coins) => this.coins -= coins;
+
+        public float GetCoins() => coins;
+    }
+}
