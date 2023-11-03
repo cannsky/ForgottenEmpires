eb9d63f256e82bd2defa512a5f87f0fe850bde22
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-03 20:49:52 +0300

    add: non playable character animator

---
+++ Unity/Assets/Scripts/Entity/Element/Non Playable Character/Worker/Non Playable Character Animator/NonPlayableCharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ForgottenEmpires.Entity.Elements.NonPlayableCharacters.Workers
+{
+    public class NonPlayableCharacterAnimator
+    {
+        private NonPlayableCharacterWorker nonPlayableCharacterWorker;
+
+        private Animator animator;
+
+        public NonPlayableCharacterAnimator(NonPlayableCharacterWorker nonPlayableCharacterWorker)
+        {
+            this.nonPlayableCharacterWorker = nonPlayableCharacterWorker;
+            animator = nonPlayableCharacterWorker.nonPlayableCharacter.GetComponent<Animator>();
+        }
+
+        public void OnStart() => nonPlayableCharacterWorker.nonPlayableCharacter.StartCoroutine(StartAnimationAtRandomSecond());
+
+        public IEnumerator StartAnimationAtRandomSecond()
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 1f));
+            animator.SetBool("Idle", true);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Entity/Element/Non Playable Character/Worker/Non Playable Character Start/NonPlayableCharacterStart.cs
@@ -0,0 +1,19 @@
+﻿namespace ForgottenEmpires.Entity.Elements.NonPlayableCharacters.Workers
+{
+    public class NonPlayableCharacterStart
+    {
+        public NonPlayableCharacterWorker nonPlayableCharacterWorker;
+
+        public NonPlayableCharacterStart(NonPlayableCharacterWorker nonPlayableCharacterWorker) => this.nonPlayableCharacterWorker = nonPlayableCharacterWorker;
+
+        public void OnStart()
+        {
+            if (nonPlayableCharacterWorker.nonPlayableCharacter.isClient) OnClientStart();
+            if (nonPlayableCharacterWorker.nonPlayableCharacter.isServer) OnServerStart();
+        }
+
+        public void OnClientStart() { }
+
+        public void OnServerStart() { }
+    }
+}
