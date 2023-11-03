f5d75e617c9fe10ca5724fb1b1c2013d08d180ea
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-03 20:49:32 +0300

    add: non playable character & non playable character worker

---
+++ Unity/Assets/Scripts/Entity/Element/Non Playable Character/NonPlayableCharacter.cs
@@ -0,0 +1,28 @@
+﻿using ForgottenEmpires.Entity.Elements.NonPlayableCharacters.Workers;
+using ForgottenEmpires.Types;
+
+namespace ForgottenEmpires.Entity.Elements.NonPlayableCharacters
+{
+    public class NonPlayableCharacter : Element
+    {
+        public NonPlayableCharacterWorker nonPlayableCharacterWorker;
+
+        private void Start()
+        {
+            nonPlayableCharacterWorker = new NonPlayableCharacterWorker(this);
+            nonPlayableCharacterWorker.OnStart();
+        }
+
+        public override void SetAnimation(AnimationType animationType, bool value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void TakeDamage(float damage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Unity/Assets/Scripts/Entity/Element/Non Playable Character/Worker/NonPlayableCharacterWorker.cs
@@ -0,0 +1,22 @@
+﻿namespace ForgottenEmpires.Entity.Elements.NonPlayableCharacters.Workers
+{
+    public class NonPlayableCharacterWorker
+    {
+        public NonPlayableCharacter nonPlayableCharacter;
+
+        public NonPlayableCharacterStart nonPlayableCharacterStart;
+
+        public NonPlayableCharacterAnimator nonPlayableCharacterAnimator;
+
+        public NonPlayableCharacterWorker(NonPlayableCharacter nonPlayableCharacter)
+        {
+            this.nonPlayableCharacter = nonPlayableCharacter;
+
+            nonPlayableCharacterStart = new NonPlayableCharacterStart(this);
+
+            nonPlayableCharacterAnimator = new NonPlayableCharacterAnimator(this);
+        }
+
+        public void OnStart() => nonPlayableCharacterStart.OnStart();
+    }
+}
