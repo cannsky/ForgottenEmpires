0b724b718075e4e9c1bde56fd62cc339ea449992
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-04 23:09:25 +0300

    add: spawner worker

---
+++ Unity/Assets/Scripts/Spawner/Worker/SpawnerWorker.cs
@@ -0,0 +1,26 @@
+﻿namespace ForgottenEmpires.Spawners.Workers
+{
+    public class SpawnerWorker
+    {
+        public Spawner spawner;
+
+        public SpawnerStart spawnerStart;
+        public SpawnerUpdate spawnerUpdate;
+
+        public SpawnerGenerator spawnerGenerator;
+
+        public SpawnerWorker(Spawner spawner)
+        {
+            this.spawner = spawner;
+
+            spawnerStart = new SpawnerStart(this);
+            spawnerUpdate = new SpawnerUpdate(this);
+
+            spawnerGenerator = new SpawnerGenerator(this);
+        }
+
+        public void OnStart() => spawnerStart.OnStart();
+
+        public void OnUpdate() => spawnerUpdate.OnUpdate();
+    }
+}
