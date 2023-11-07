39876021a9aa593e42ac722ebd4306ff4219638d
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-08 02:41:29 +0300

    fix: player continous movement

--- Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Movement/PlayerMovement.cs
+++ Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Movement/PlayerMovement.cs
@@ -11,13 +11,26 @@ namespace ForgottenEmpires.Entity.Elements.PlayerWorkers
 
         private Vector3 velocity, normalVector;
 
-        public PlayerMovement(PlayerWorker playerWorker) => this.playerWorker = playerWorker;
+        public PlayerMovement(PlayerWorker playerWorker)
+        {
+            this.playerWorker = playerWorker;
+            rigidbody = playerWorker.player.GetComponent<Rigidbody>();
+        }
 
         public void OnUpdate() => Move();
 
-        public void SetMovementPosition(Vector2 position) => velocity = Vector3.ProjectOnPlane(position, normalVector);
+        public void SetMovementPosition(Vector2 position) => velocity = Vector3.ProjectOnPlane(new Vector3(position.x, 0, position.y), normalVector);
 
-        //TODO: CONTINOUS MOVEMENT BUG, FIX HERE.
-        public void Move() => rigidbody.velocity = velocity;
+        public void Move()
+        {
+            rigidbody.velocity = velocity;
+            ApplyFriction();
+        }
+
+        public void ApplyFriction()
+        {
+            if (velocity.magnitude > 0f) velocity -= velocity * 0.5f;
+            else if (velocity.magnitude < 0f) velocity = Vector3.zero;
+        }
     }
 }
