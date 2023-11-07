fcd28a8a064339163ad928d8baca01dbd0ed386c
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-08 02:22:19 +0300

    add: player camera follow

---
+++ Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Camera/PlayerCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace ForgottenEmpires.Entity.Elements.PlayerWorkers
+{
+    public class PlayerCamera
+    {
+        private PlayerWorker playerWorker;
+        private Transform cameraTransform;
+        private Vector3 offset = new Vector3(0, 7, -4);
+        private float angle = 60;
+
+        public PlayerCamera (PlayerWorker playerWorker) => this.playerWorker = playerWorker;
+
+        public void OnStart() => FocusMainCameraToOwnerPlayer();
+
+        public void OnUpdate()
+        {
+            FollowOwnerPlayer();
+            RotateCamera();
+        }
+
+        public void FocusMainCameraToOwnerPlayer()
+        {
+            cameraTransform = Camera.main.transform;
+            cameraTransform.position = playerWorker.player.transform.position + offset;
+            cameraTransform.rotation = Quaternion.Euler(angle, 0, 0);
+        }
+
+        public void FollowOwnerPlayer() => cameraTransform.position = playerWorker.player.transform.position + offset;
+
+        public void RotateCamera()
+        {
+
+        }
+    }
+}
