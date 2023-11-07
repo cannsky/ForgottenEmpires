3ab2ac11c52b312421c195e0ae910d83b60dce79
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-08 02:23:33 +0300

    add: player start

---
+++ Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Start/PlayerStart.cs
@@ -0,0 +1,30 @@
+﻿namespace ForgottenEmpires.Entity.Elements.PlayerWorkers
+{
+    public class PlayerStart
+    {
+        private PlayerWorker playerWorker;
+
+        public PlayerStart(PlayerWorker playerWorker) => this.playerWorker = playerWorker;
+
+        public void OnStart()
+        {
+            if (playerWorker.player.isClient) ClientOnStart();
+            if (playerWorker.player.isServer) ServerOnStart();
+        }
+
+        public void ClientOnStart()
+        {
+            if (playerWorker.player.isLocalPlayer) OwnerClientOnStart();
+        }
+
+        public void OwnerClientOnStart()
+        {
+            playerWorker.playerCamera.OnStart();
+        }
+
+        public void ServerOnStart()
+        {
+
+        }
+    }
+}
