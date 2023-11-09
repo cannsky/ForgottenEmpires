736364706282c2960332aa5afaddc8d0418f7218
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 16:53:54 +0300

    add: js connector & js manager jslib

---
+++ Unity/Assets/Scripts/Modules/Connector/JS/JSConnector.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ForgottenEmpires.Managers.JS
+{
+    public class JSConnector
+    {
+        public static JSConnector Instance;
+
+        [DllImport("__Internal")]
+        private static extern void BuyPotion(string callbackObjectName, string callbackMethodName);
+
+        [DllImport("__Internal")]
+        private static extern void UsePotion(string callbackObjectName, string callbackMethodName);
+
+        [DllImport("__Internal")]
+        private static extern void ConnectWallet(string callbackObjectName, string callbackMethodName);
+
+        public void ReturnMessage(string message)
+        {
+            string[] keyValue = message.Split(':');
+
+            if (keyValue.Length != 2) return;
+            string category = keyValue[0].Trim();
+            int value = int.Parse(keyValue[1].Trim());
+
+            switch (category)
+            {
+                case "wallet":
+                    ProcessWallet(value);
+                    break;
+                case "potionUse":
+                    ProcessPotionUse(value);
+                    break;
+                case "potionBuy":
+                    ProcessPotionBuy(value);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ProcessWallet(int value)
+        {
+
+        }
+
+        private void ProcessPotionUse(int value)
+        {
+
+        }
+
+        private void ProcessPotionBuy(int value)
+        {
+
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Modules/Connector/JS/Plugins/JSManager.jslib
@@ -0,0 +1,21 @@
+﻿mergeInto(LibraryManager.library, {
+
+  BuyPotion: async function(callbackObjectNameStr, callbackMethodNameStr) {
+	const callbackObjectName = UTF8ToString(callbackObjectNameStr);
+	const callbackMethodName = UTF8ToString(callbackMethodNameStr);
+	unityGameInstance.SendMessage(callbackObjectName, callbackMethodName, "wallet:0");
+  },
+
+  UsePotion: async function(callbackObjectNameStr, callbackMethodNameStr) {
+  	const callbackObjectName = UTF8ToString(callbackObjectNameStr);
+	const callbackMethodName = UTF8ToString(callbackMethodNameStr);
+	unityGameInstance.SendMessage(callbackObjectName, callbackMethodName, "wallet:0");
+  },
+
+  ConnectWallet: async function(callbackObjectNameStr, callbackMethodNameStr) {
+  	const callbackObjectName = UTF8ToString(callbackObjectNameStr);
+	const callbackMethodName = UTF8ToString(callbackMethodNameStr);
+	unityGameInstance.SendMessage(callbackObjectName, callbackMethodName, "wallet:0");
+  }
+
+});
