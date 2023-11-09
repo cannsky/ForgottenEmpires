e1a7feb365a0a649625b2894470c5ddb6b2b1269
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:21:01 +0300

    add: merchant worker

---
+++ Unity/Assets/Scripts/Entity/Element/NPC/Merchant/Worker/MerchantWorker.cs
@@ -0,0 +1,28 @@
+﻿namespace ForgottenEmpires.Entities.Elements.NPCs.Workers
+{
+    public class MerchantWorker
+    {
+        public Merchant merchant;
+
+        public MerchantUpdate merchantUpdate;
+
+        public MerchantInteraction merchantInteraction;
+        public MerchantTrade merchantTrade;
+        public MerchantTrigger merchantTrigger;
+        public MerchantUI merchantUI;
+
+        public MerchantWorker(Merchant merchant)
+        {
+            this.merchant = merchant;
+
+            merchantUpdate = new MerchantUpdate(this);
+
+            merchantInteraction = new MerchantInteraction(this);
+            merchantTrade = new MerchantTrade(this);
+            merchantTrigger = new MerchantTrigger(this);
+            merchantUI = new MerchantUI(this);
+        }
+
+        public void OnUpdate() => merchantUpdate.OnUpdate();
+    }
+}
