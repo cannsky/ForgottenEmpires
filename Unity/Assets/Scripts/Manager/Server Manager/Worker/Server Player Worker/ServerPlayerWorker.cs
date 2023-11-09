57496387b9b3281fd25d9ff20b10699624769d5b
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 16:53:03 +0300

    fix: server player dictionary empty player problem

--- Unity/Assets/Scripts/Manager/Server Manager/Worker/Server Player Worker/ServerPlayerWorker.cs
+++ Unity/Assets/Scripts/Manager/Server Manager/Worker/Server Player Worker/ServerPlayerWorker.cs
@@ -5,11 +5,13 @@ namespace ForgottenEmpires.Managers.Server.Workers
 {
     public class ServerPlayerWorker
     {
+        public static uint playerCount;
+
         public Dictionary<uint, Element> players;
 
         public ServerPlayerWorker() => players = new Dictionary<uint, Element>();
 
-        public void AddPlayer(uint id) => players.Add(id, new Player());
+        public void AddPlayer(Player player) => players.Add(playerCount++, player);
 
         public void RemovePlayer(uint id) => players.Remove(id);
     }
