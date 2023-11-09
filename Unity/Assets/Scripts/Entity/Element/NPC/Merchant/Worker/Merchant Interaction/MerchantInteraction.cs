4a834199d0e98a007d169fbc958a10e60f327ee7
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:21:47 +0300

    add: merchant interaction

---
+++ Unity/Assets/Scripts/Entity/Element/NPC/Merchant/Worker/Merchant Interaction/MerchantInteraction.cs
@@ -0,0 +1,11 @@
+﻿namespace ForgottenEmpires.Entities.Elements.NPCs.Workers
+{
+    public class MerchantInteraction
+    {
+        private MerchantWorker merchantWorker;
+
+        public MerchantInteraction(MerchantWorker merchantWorker) => this.merchantWorker = merchantWorker;
+
+        public void OnInteraction(Element element) => merchantWorker.merchantUI.ToggleUI();
+    }
+}
