e9f9c24bc47c4cd190d7b00c7b4a057d1d11080a
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:20:54 +0300

    add: merchant

---
+++ Unity/Assets/Scripts/Entity/Element/NPC/Merchant/Merchant.cs
@@ -0,0 +1,36 @@
+﻿using ForgottenEmpires.Entities.Elements.NPCs.Workers;
+using ForgottenEmpires.Types;
+using UnityEngine;
+
+namespace ForgottenEmpires.Entities.Elements.NPCs
+{
+    public class Merchant : NPC
+    {
+        private MerchantWorker merchantWorker;
+
+        private void Start() => merchantWorker = new MerchantWorker(this);
+
+        private void Update() => merchantWorker.OnUpdate();
+
+        private void OnTriggerEnter(Collider other) => merchantWorker.merchantTrigger.OnTriggerEnter(other);
+
+        private void OnTriggerExit(Collider other) => merchantWorker.merchantTrigger.OnTriggerExit(other);
+
+        public override void Regenerate()
+        {
+            
+        }
+
+        public override void SetAnimation(AnimationType animationType, bool value)
+        {
+            
+        }
+
+        public override void TakeDamage(float damage)
+        {
+            
+        }
+
+        public override void Interact(Element element) => merchantWorker.merchantInteraction.OnInteraction(element);
+    }
+}
