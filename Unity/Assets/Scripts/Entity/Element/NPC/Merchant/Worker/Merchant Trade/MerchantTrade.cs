afb49fe94815b292f598565ff1362a6995f73d1e
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:22:36 +0300

    add: merchant trade

---
+++ Unity/Assets/Scripts/Entity/Element/NPC/Merchant/Worker/Merchant Trade/MerchantTrade.cs
@@ -0,0 +1,11 @@
+﻿namespace ForgottenEmpires.Entities.Elements.NPCs.Workers
+{
+    public class MerchantTrade
+    {
+        private MerchantWorker merchantWorker;
+
+        public MerchantTrade(MerchantWorker merchantWorker) => this.merchantWorker = merchantWorker;
+
+
+    }
+}
