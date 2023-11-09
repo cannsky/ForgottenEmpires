bd6ec6d4ba8afba43920e83dfd142a2522f1795a
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:22:06 +0300

    add: merchant ui

---
+++ Unity/Assets/Scripts/Entity/Element/NPC/Merchant/Worker/Merchant UI/MerchantUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ForgottenEmpires.Entities.Elements.NPCs.Workers
+{
+    public class MerchantUI
+    {
+        private MerchantWorker merchantWorker;
+
+        public GameObject merchantUIGameObject;
+
+        public MerchantUI(MerchantWorker merchantWorker)
+        {
+            this.merchantWorker = merchantWorker;
+            merchantUIGameObject = GameObject.Find("Merchant Canvas");
+        }
+
+        public void ToggleUI() => merchantUIGameObject.SetActive(!merchantUIGameObject.activeSelf);
+    }
+}
