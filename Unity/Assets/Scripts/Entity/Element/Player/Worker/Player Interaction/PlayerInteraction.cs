cb7d9419deecf002ac604cb78c0145c862ce9e5c
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:23:49 +0300

    add: player interaction

---
+++ Unity/Assets/Scripts/Entity/Element/Player/Worker/Player Interaction/PlayerInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ForgottenEmpires.Entities.Elements.PlayerWorkers
+{
+    public class PlayerInteraction
+    {
+        private PlayerWorker playerWorker;
+
+        public Element interactable;
+
+        public PlayerInteraction(PlayerWorker playerWorker) => this.playerWorker = playerWorker;
+
+        public void OnUpdate()
+        {
+            if (!playerWorker.playerInput.interact) return;
+            interactable.Interact(playerWorker.player);
+        }
+    }
+}
