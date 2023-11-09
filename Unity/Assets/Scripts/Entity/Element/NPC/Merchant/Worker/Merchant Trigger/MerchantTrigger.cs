52dc578c7f183145ac4d7f1bf0c9c9e788931f97
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-09 23:21:58 +0300

    add: merchant trigger

---
+++ Unity/Assets/Scripts/Entity/Element/NPC/Merchant/Worker/Merchant Trigger/MerchantTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ForgottenEmpires.Entities.Elements.NPCs.Workers
+{
+    public class MerchantTrigger
+    {
+        private MerchantWorker merchantWorker;
+
+        public MerchantTrigger(MerchantWorker merchantWorker) => this.merchantWorker = merchantWorker;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.tag != "Player") return;
+            other.GetComponent<Player>().playerWorker.playerInteraction.interactable = merchantWorker.merchant;
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (other.tag != "Player") return;
+            other.GetComponent<Player>().playerWorker.playerInteraction.interactable = null;
+        }
+    }
+}
