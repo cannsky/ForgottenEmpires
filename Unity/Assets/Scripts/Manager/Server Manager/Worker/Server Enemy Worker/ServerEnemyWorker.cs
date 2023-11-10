6c7316aedcd62f062cfc9f512aa49746d02f3002
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-10 03:09:00 +0300

    add: server enemy worker & enemies list

---
+++ Unity/Assets/Scripts/Manager/Server Manager/Worker/Server Enemy Worker/ServerEnemyWorker.cs
@@ -0,0 +1,19 @@
+﻿using ForgottenEmpires.Entities.Elements;
+using ForgottenEmpires.Entities.Elements.Enemies;
+using System.Collections.Generic;
+
+namespace ForgottenEmpires.Managers.Server.Workers
+{
+    public class ServerEnemyWorker
+    {
+        public static uint enemyCount;
+
+        public Dictionary<uint, Element> enemies;
+
+        public ServerEnemyWorker() => enemies = new Dictionary<uint, Element>();
+
+        public void AddEnemy(Enemy enemy) => enemies.Add(enemyCount++, enemy);
+
+        public void RemoveEnemy(uint id) => enemies.Remove(id);
+    }
+}
--- Unity/Assets/Scripts/Manager/Server Manager/Worker/ServerManagerWorker.cs
+++ Unity/Assets/Scripts/Manager/Server Manager/Worker/ServerManagerWorker.cs
@@ -2,11 +2,13 @@ namespace ForgottenEmpires.Managers.Server.Workers
 {
     public class ServerManagerWorker
     {
+        public ServerEnemyWorker serverEnemyWorker;
         public ServerPlayerWorker serverPlayerWorker;
         public ServerStartWorker serverStartWorker;
 
         public ServerManagerWorker()
         {
+            serverEnemyWorker = new ServerEnemyWorker();
             serverPlayerWorker = new ServerPlayerWorker();
             serverStartWorker = new ServerStartWorker();
         }
