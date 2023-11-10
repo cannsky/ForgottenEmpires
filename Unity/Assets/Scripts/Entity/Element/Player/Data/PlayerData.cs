631b20297ec18161ace4d9f456d6b383d9c8603a
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-10 17:43:42 +0300

    add: player potion count functionality to player data

--- Unity/Assets/Scripts/Entity/Element/Player/Data/PlayerData.cs
+++ Unity/Assets/Scripts/Entity/Element/Player/Data/PlayerData.cs
@@ -1,8 +1,24 @@
-﻿namespace ForgottenEmpires.Entities.Elements.PlayerDatas
+﻿using ForgottenEmpires.Managers.Data;
+using System.Collections;
+using UnityEngine;
+
+namespace ForgottenEmpires.Entities.Elements.PlayerDatas
 {
     public class PlayerData
     {
-        public float armor;
-        public float attackDamage;
+        private Player player;
+
+        public float potionCount;
+
+        public void UpdatePlayerData() => potionCount = DataManager.Instance.GetPlayerData(player.walletAddress).potionCount;
+
+        public void OnStart() => player.StartCoroutine(GetPlayerData());
+
+        public IEnumerator GetPlayerData()
+        {
+            yield return new WaitForSeconds(10f);
+            UpdatePlayerData();
+            player.StartCoroutine(GetPlayerData());
+        }
     }
 }
