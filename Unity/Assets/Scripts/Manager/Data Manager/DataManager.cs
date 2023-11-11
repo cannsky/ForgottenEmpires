103b553057b8134c30e88c1da7efc27eceb21988
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-11 05:08:35 +0300

    fix: data manager null pointer exception problem

--- Unity/Assets/Scripts/Manager/Data Manager/DataManager.cs
+++ Unity/Assets/Scripts/Manager/Data Manager/DataManager.cs
@@ -1,52 +1,80 @@
-﻿using ForgottenEmpires.Managers.Data.Components;
+﻿using Newtonsoft.Json;
+using ForgottenEmpires.Managers.Data.Components;
 using ForgottenEmpires.Managers.Server;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Linq;
 
 namespace ForgottenEmpires.Managers.Data
 {
     public class DataManager : MonoBehaviour
     {
+        public bool isServer;
+
         private MerkleTree merkleTree;
 
         public static DataManager Instance;
 
-        private string url = "https://forgottenempires.network/"; //Update here with your domain name.
+        private string url = "http://localhost:4321/";
 
         private void Awake()
         {
             Instance = this;
         }
 
-        private void Start() => ServerManager.Instance.StartCoroutine(UpdateMerkleTree());
+        private void Start()
+        {
+            Debug.Log("IsServer: " + isServer);
+            if (isServer) ServerManager.Instance.StartCoroutine(UpdateMerkleTree());
+            merkleTree = new MerkleTree();
+        }
 
         public IEnumerator UpdateMerkleTree()
         {
-            yield return new WaitForSeconds(5f);
-            yield return ServerManager.Instance.StartCoroutine(SendPostRequest());
-            UpdateMerkleTree();
+            while (true)
+            {
+                yield return SendPostRequest();
+                yield return new WaitForSeconds(20f);
+            }
         }
 
         public IEnumerator SendPostRequest()
         {
-            using (UnityWebRequest webRequest = new UnityWebRequest(url))
+            Debug.Log("Data is being updated...");
+            UnityWebRequest webRequest = new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST)
             {
-                webRequest.downloadHandler = new DownloadHandlerBuffer();
+                downloadHandler = new DownloadHandlerBuffer(),
+                uploadHandler = new UploadHandlerRaw(new byte[0])
+            };
+            webRequest.SetRequestHeader("Content-Type", "application/json");
 
-                yield return webRequest.SendWebRequest();
+            yield return webRequest.SendWebRequest();
 
-                if (webRequest.result != UnityWebRequest.Result.Success) Debug.LogError("Error: " + webRequest.error);
-                else
-                {
-                    string jsonResult = webRequest.downloadHandler.text;
-                    merkleTree = JsonUtility.FromJson<MerkleTree>("{\"items\":" + jsonResult + "}");
-                }
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error: " + webRequest.error);
             }
+            else
+            {
+                string jsonResult = webRequest.downloadHandler.text;
+                List<MerkleTreeNode> nodes = JsonConvert.DeserializeObject<List<MerkleTreeNode>>(jsonResult);
+                merkleTree.UpdateNodes(nodes);
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+
+            webRequest.Dispose();
         }
 
-        public MerkleTreeNode GetPlayerData(string walletAddress) => merkleTree.nodes.FirstOrDefault(playerData => playerData.walletAddress == walletAddress);
+        public MerkleTreeNode GetPlayerData(string walletAddress)
+        {
+            if (merkleTree == null || merkleTree.nodes == null)
+            {
+                //Debug.Log("Merkle tree or nodes are null");
+                return null;
+            }
+            return merkleTree.nodes.FirstOrDefault(playerData => playerData.publicKey == walletAddress);
+        }
     }
 }
