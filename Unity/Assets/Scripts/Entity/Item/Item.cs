3c1e1ff20b6c76aac8620aac683e24584472935d
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-22 21:50:27 +0300

    add: cooldown, effect return to item

--- Unity/Assets/Scripts/Entity/Item/Item.cs
+++ Unity/Assets/Scripts/Entity/Item/Item.cs
@@ -1,10 +1,37 @@
 ﻿using ForgottenEmpires.Components.Effects;
+using ForgottenEmpires.Entities.Elements;
 using System.Collections;
+using UnityEngine;
+using static UnityEditor.Progress;
 
 namespace ForgottenEmpires.Entities.Items
 {
     public abstract class Item
     {
+        // Owner of the item
+        public Element owner;
+
+        // Effect of the item
         public Effect effect;
+
+        // Cooldown time for item usage
+        public float cooldown;
+
+        // Is item in cooldown
+        public bool isCooldown;
+
+        public virtual Effect GetEffect()
+        {
+            if (isCooldown) return null;
+            ResetCooldown();
+            return effect;
+        }
+
+        public virtual IEnumerator ResetCooldown()
+        {
+            isCooldown = true;
+            yield return new WaitForSeconds(cooldown);
+            isCooldown = false;
+        }
     }
 }
