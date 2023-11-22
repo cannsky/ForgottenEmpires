53f36d42b3061c9bb01e1e66e57167da82a13107
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-22 21:29:51 +0300

    add: element stats & element event

---
+++ Unity/Assets/Scripts/Entity/Element/Worker/Element Event/ElementEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace ForgottenEmpires.Entities.Elements.Workers
+{
+    public class ElementEvent
+    {
+        private ElementWorker elementWorker;
+
+        public ElementEvent(ElementWorker elementWorker) => this.elementWorker = elementWorker;
+
+        public void DeathEvent()
+        {
+            // TODO: DISABLE PLAYER, RESPAWN ETC.
+        }
+    }
+}
--- Unity/Assets/Scripts/Entity/Element/Worker/Element Stats/ElementStats.cs
+++ Unity/Assets/Scripts/Entity/Element/Worker/Element Stats/ElementStats.cs
@@ -4,8 +4,17 @@
     {
         private ElementWorker elementWorker;
 
+        private float health, totalHealth;
+
         public ElementStats(ElementWorker elementWorker) => this.elementWorker = elementWorker;
 
-
+        // Change element health value
+        public void UpdateHealth(float value)
+        {
+            // If increased health value is greater than total health, health should be total health
+            // If decreased health value is lower than or equal to 0, element death event will be triggered.
+            if (value > 0 && (health += value) > totalHealth) health = totalHealth;
+            else if ((health += value) <= 0) elementWorker.elementEvent.DeathEvent();
+        }
     }
 }
