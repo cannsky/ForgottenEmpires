424c31cc2afb73b0e84c63e287c13975ccbc61c9
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-11-26 23:16:34 +0300

    add: npc animation

---
+++ Unity/Assets/Scripts/Entity/Element/Component/NPC/Worker/NPC Animation/NPCAnimation.cs
@@ -0,0 +1,37 @@
+﻿using ForgottenEmpires.Entities.Elements.PlayerWorkers;
+using ForgottenEmpires.Types;
+using UnityEngine;
+
+namespace ForgottenEmpires.Entities.Elements.NPCs.Workers
+{
+    public class NPCAnimation
+    {
+        private NPCWorker npcWorker;
+
+        // Animator component for npc animations.
+        public Animator animator;
+
+        public NPCAnimation(NPCWorker npcWorker)
+        {
+            this.npcWorker = npcWorker;
+
+            // Get the Animator component from the npc gameobject.
+            animator = npcWorker.npc.GetComponent<Animator>();
+        }
+
+        // Reset animator parameters
+        public void ResetAnimator()
+        {
+
+        }
+
+        public void SetAnimation(AnimationType animationType, bool value)
+        {
+            // Reset animator
+            ResetAnimator();
+
+            // Set animation to given value
+            animator.SetBool(animationType.ToString(), value);
+        }
+    }
+}
