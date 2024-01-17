1b7d88885d7fa0923902d628a9ffefffcb2fd003
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-18 02:22:29 +0300

    add: player on chain data & update player data

---
+++ Unity/Entity/Element/Component/Player/Data/Player On Chain Data/PlayerOnChainData.cs
@@ -0,0 +1,30 @@
+using ForgottenEmpires.Managers.Data;
+using ForgottenEmpires.Managers.Data.Components;
+using System.Collections;
+using UnityEngine;
+
+namespace ForgottenEmpires.Entities.Elements.PlayerDatas
+{
+    public class PlayerOnChainData
+    {
+        private PlayerData playerData;
+
+        public PlayerOnData(PlayerData playerData)
+        {
+            this.playerData = playerData;
+        }
+
+        // Update the player's data by data of DataManager.
+        public void UpdatePlayerData()
+        {
+            // Check if the player or wallet address is null
+            if (playerData.player == null || playerData.player.walletAddress == null) return;
+
+            // Retrieve the player's data from the DataManager.
+            MerkleTreeNode node = DataManager.Instance.GetPlayerData(playerData.player.walletAddress);
+
+            // Update the player's potion count if data was found.
+            if (node != null) playerData.player.playerWorker.playerStats.playerOnChainStats.UpdateOnChainData(node.potionCount);
+        }
+    }
+}
--- Unity/Entity/Element/Component/Player/Data/PlayerData.cs
+++ Unity/Entity/Element/Component/Player/Data/PlayerData.cs
@@ -9,39 +9,21 @@ namespace ForgottenEmpires.Entities.Elements.PlayerDatas
     {
         private Player player;
 
+        public PlayerOnChainData playerOnChainData;
+        public PlayerOffChainData playerOffChainData;
+
         public PlayerData(Player player)
         {
             this.player = player;
-        }
 
-        // Update the player's data by data of DataManager.
-        public void UpdatePlayerData()
-        {
-            // Check if the player or wallet address is null
-            if (player == null || player.walletAddress == null) return;
-
-            // Retrieve the player's data from the DataManager.
-            MerkleTreeNode node = DataManager.Instance.GetPlayerData(player.walletAddress);
-
-            // Update the player's potion count if data was found.
-            if (node != null) player.playerWorker.playerStats.playerOnChainStats.UpdateOnChainData(node.potionCount);
+            playerOnChainData = new PlayerOnChainData(this);
+            playerOffChainData = new PlayerOffChainData(this);
         }
 
         public void OnStart()
         {
             // Start a coroutine to continously retrieve player data.
-            player.StartCoroutine(GetPlayerData());
-        }
-
-        public IEnumerator GetPlayerData()
-        {
-            yield return new WaitForSeconds(10f);
-
-            // Update the player's data.
-            UpdatePlayerData();
-
-            // Start a coroutine to continously retrieve player data.
-            player.StartCoroutine(GetPlayerData());
+            DataManager.Instance.playerDataManager.playerOnChainDataManager.RegisterPlayer(playerOnChainData);
         }
     }
 }
