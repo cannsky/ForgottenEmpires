624e7f767a4d78a9b1a5559c4d56a7b575c571fc
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-18 02:27:56 +0300

    add: player off chain data manager

---
+++ Unity/Manager/Data Manager/Player Data Manager/Player Off Chain Data Manager/PlayerOffChainDataManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForgottenEmpires.Managers.Data
+{
+    public class PlayerOffChainDataManager
+    {
+        
+    }
+}
