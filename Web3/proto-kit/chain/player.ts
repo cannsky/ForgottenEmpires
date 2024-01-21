937923588b6bf61be74d5e21d7ebdec78a57e020
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-22 00:15:50 +0300

    add: player addxp method

--- Web3/proto-kit/chain/player.ts
+++ Web3/proto-kit/chain/player.ts
@@ -1,3 +1,6 @@
+// This code is not completed.
+// This code is not audited.
+
 import {
     RuntimeModule,
     runtimeModule,
@@ -6,23 +9,47 @@ import {
 } from "@proto-kit/module";
 
 import {
-    StateMap
+    StateMap,
+    assert
 } from "@proto-kit/protocol";
 
 import {
+    Provable,
     PublicKey,
     Struct,
-    UInt32
+    UInt64
 } from "o1js";
 
 export class PlayerEntity extends Struct({
-    xp: UInt32
+    level: UInt64,
+    xp: UInt64,
 }) {}
 
 @runtimeModule()
 export class Player extends RuntimeModule<{}> {
 
     @state() public players = StateMap.from<PublicKey, PlayerEntity>(PublicKey, PlayerEntity);
+
+    // For testing
+    @runtimeMethod()
+    public addXP(address: PublicKey, amount: UInt64) {
+        // Get player
+        const player = this.players.get(address).value;
+        // Get current xp value of the player
+        const currentXP = player.xp;
+        // Set new xp value of the player
+        const newXP = currentXP.value.add(amount);
+        // Set the new xp of the player
+        this.players.set(
+            address, 
+            new PlayerEntity({ xp: newXP, level: player.level })
+        );
+    }
+
+    @runtimeMethod()
+    public levelUP(address: PublicKey) {
+        
+    }
 
     // methods will be added later...
 }
