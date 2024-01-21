d58e1594c02bb8a240a83cc67603cdfb6b65237e
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-21 22:27:20 +0300

    add: onchain character

---
+++ Web3/proto-kit/chain/character.ts
@@ -0,0 +1,33 @@
+import {
+    RuntimeModule,
+    runtimeModule,
+    state,
+    runtimeMethod
+} from "@proto-kit/module";
+
+import {
+    StateMap
+} from "@proto-kit/protocol";
+
+import {
+    PublicKey,
+    Struct,
+    UInt32
+} from "o1js";
+
+export class CharacterKey extends Struct({
+    owner: PublicKey,
+    id: UInt32,
+}) {}
+
+export class CharacterEntity extends Struct({
+    xp: UInt32,
+}) {}
+
+@runtimeModule()
+export class Character extends RuntimeModule<{}> {
+
+    @state() public playerStats = StateMap.from<CharacterKey, CharacterEntity>(CharacterKey, CharacterEntity);
+
+    // methods will be added later...
+}
