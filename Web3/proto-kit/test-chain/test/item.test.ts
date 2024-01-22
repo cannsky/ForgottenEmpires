f61c1836a0916880328e7d75b044be96ec34a076
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-23 01:43:11 +0300

    add: item test

---
+++ Web3/proto-kit/test-chain/test/item.test.ts
@@ -0,0 +1,46 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+
+import { PrivateKey, UInt32 } from "o1js";
+
+import { Item, EquippedItemKey } from "../item";
+
+import { log } from "@proto-kit/common";
+
+log.setLevel("error");
+
+describe("Item", () => {
+    it("should demonstrate how item work", async () => {
+        const appChain = TestingAppChain.fromRuntime({
+            modules: {
+                Item,
+            },
+            config: {
+                Item: {},
+            },
+        });
+
+        await appChain.Start();
+
+        const alicePrivateKey = PrivateKey.random();
+        const alice = alicePrivateKey.toPublicKey();
+
+        appChain.setSigner(alicePrivateKey);
+
+        const player = appChain.runtime.resolve("Player");
+
+        const tx1 = await appChain.transaction(alice, () => {
+            player.equipItem(alice, UInt32.from(1), UInt32.from(1));
+        });
+
+        await tx1.sign();
+        await tx1.send();
+
+        const block1 = await appChain.produceBlock();
+
+        let aliceItem = await appChain.query.runtime.Item.items.get(new EquippedItemKey( owner: alice, slot: UInt32.from(1) })).value.itemid;
+
+        expect(block1?.txs[0].status).toBe(true);
+
+        expect(aliceXP?.toBigInt()).toBe(1n);
+    });
+});
