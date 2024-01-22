7a414ceafeca071b4c68c2d9d9ccacf4cbd3e3de
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-23 01:30:12 +0300

    add: onchain equip & unequip functionality to item.

--- Web3/proto-kit/test-chain/item.ts
+++ Web3/proto-kit/test-chain/item.ts
@@ -1,3 +1,6 @@
+// This code is not completed.
+// This code is not audited.
+
 import {
     RuntimeModule,
     runtimeModule,
@@ -24,10 +27,51 @@ export class ItemEntity extends Struct({
     damage: UInt32,
 }) {}
 
+export class EquipedItemKey extends Struct({
+    owner: PublicKey,
+    slot: UInt32
+}) {}
+
+export class EquipedItemEntity extends Struct({
+    itemid: UInt32
+}) {}
+
 @runtimeModule()
 export class Item extends RuntimeModule<{}> {
 
-    @state() public itemStats = StateMap.from<ItemKey, ItemEntity>(ItemKey, ItemEntity);
+    @state() public items = StateMap.from<ItemKey, ItemEntity>(ItemKey, ItemEntity);
+
+    @state() public equipedItems = StateMap.from<EquipedItemKey, EquipedItemEntity>(EquipedItemKey, EquipedItemEntity);
+
+    @runtimeMethod()
+    public equipItem(address: PublicKey, equipeditemslot: UInt32, itemid: UInt32) {
+        // Get inventory slot
+        const equipmentSlot = this.equipedItems.get(new EquipedItemKey({ owner: address, slot: equipeditemslot }).value;
+        // Get current item id of the inventory slot
+        const currentItemID = equipmentSlot.itemid;
+        // If this slot is full return error
+        assert(currentItemID.value.greaterThanOrEqual(1), "This slot is already filled with an another item.");
+        // Set the new item of the equipment slot
+        this.equipedItems.set(
+            address, 
+            new EquipedItemEntity({ itemid: itemid })
+        );
+    }
+
+    @runtimeMethod()
+    public unEquipItem(address: PublicKey, equipeditemslot: UInt32) {
+        // Get inventory slot
+        const equipmentSlot = this.equipedItems.get(new EquipedItemKey({ owner: address, slot: equipeditemslot }).value;
+        // Get current item id of the inventory slot
+        const currentItemID = equipmentSlot.itemid;
+        // If this slot is full return error
+        assert(currentItemID.value.lessThanOrEqual(0), "This slot is already empty.");
+        // Unequip the item from the equipment slot
+        this.equipedItems.set(
+            address, 
+            new EquipedItemEntity({ itemid: 0 })
+        );
+    }
 
     // methods will be added later...
 }
