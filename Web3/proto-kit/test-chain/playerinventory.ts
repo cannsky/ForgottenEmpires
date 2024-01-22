96a8f788579c097398f51ed2af49ed3f8bc122c9
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-23 01:13:20 +0300

    add: on chain player inventory

---
+++ Web3/proto-kit/test-chain/playerinventory.ts
@@ -0,0 +1,58 @@
+// This code is not completed.
+// This code is not audited.
+
+import {
+    RuntimeModule,
+    runtimeModule,
+    state,
+    runtimeMethod,
+} from "@proto-kit/module";
+
+import {
+    StateMap,
+    assert
+} from "@proto-kit/protocol";
+
+import {
+    PublicKey,
+    Struct,
+    UInt32
+} from "o1js";
+
+export class InventoryKey extends Struct({
+    owner: PublicKey,
+    id: UInt32,
+}) {}
+
+export class InventoryEntity extends Struct({
+    itemid: UInt32
+}) {}
+
+@runtimeModule()
+export class PlayerInventory extends RuntimeModule<{}> {
+
+    @state() public playerInventorySlots = StateMap.from<InventoryKey, InventoryEntity>(InventoryKey, InventoryEntity);
+
+    // For testing
+    @runtimeMethod()
+    public addItem(address: PublicKey, inventoryslotid: UInt32, itemid: UInt32) {
+        // Get inventory slot
+        const inventorySlot = this.playerInventorySlots.get(new InventoryKey({ owner: address, id: inventoryslotid }).value;
+        // Get current item of the inventory slot
+        const currentItemID = inventorySlot.itemid;
+        // If this slot is full return error
+        assert(currentItemID.value.greaterThanOrEqual(1), "This slot is already filled with an another item.");
+        // Set the new item of the inventory slot
+        this.playerInventorySlots.set(
+            address, 
+            new InventoryEntity({ itemid: itemid })
+        );
+    }
+
+    @runtimeMethod()
+    public levelUP(address: PublicKey) {
+
+    }
+
+    // methods will be added later...
+}
