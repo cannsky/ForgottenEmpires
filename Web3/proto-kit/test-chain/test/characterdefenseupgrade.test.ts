f7b074967783046bdd5bdeb50fca2341b4f605a1
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-24 23:18:01 +0300

    add: character defense upgrade test

---
+++ Web3/proto-kit/test-chain/test/characterdefenseupgrade.test.ts
@@ -0,0 +1,49 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+
+import { PrivateKey, UInt32 } from "o1js";
+
+import { Character, CharacterKey } from "../character";
+
+import { log } from "@proto-kit/common";
+
+log.setLevel("error");
+
+describe("Item", () => {
+    it("should demonstrate how character work", async () => {
+        // Define appchain
+        const appChain = TestingAppChain.fromRuntime({
+            modules: {
+                Character,
+            },
+            config: {
+                Character: {},
+            },
+        });
+        // Start appchain
+        await appChain.Start();
+        // Create a random private key
+        const alicePrivateKey = PrivateKey.random();
+        // Get public key of the private key
+        const alice = alicePrivateKey.toPublicKey();
+        // Set private key
+        appChain.setSigner(alicePrivateKey);
+        // Get Character
+        const character = appChain.runtime.resolve("Character");
+        // Create a tx for testing
+        const tx1 = await appChain.transaction(alice, () => {
+            character.upgradeDefense(alice, UInt32.from(1));
+        });
+        // Sign the tx
+        await tx1.sign();
+        // Send the tx
+        await tx1.send();
+        // Produce block
+        const block1 = await appChain.produceBlock();
+        // Get the character upgraded
+        let aliceCharacterDamage = await appChain.query.runtime.Character.characters.get(new CharacterKey({ owner: alice, id: UInt32.from(1) })).value.defense;
+        // Expect block to be true
+        expect(block1?.txs[0].status).toBe(true);
+        // Expect character damage to be 1
+        expect(aliceCharacterDamage?.toBigInt()).toBe(1n);
+    });
+});
