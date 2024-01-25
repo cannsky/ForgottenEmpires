110d64937a9f73261dde2df41c2593d7c734416b
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-25 23:36:12 +0300

    add: on chain consume damage item

--- Web3/proto-kit/test-chain/item.ts
+++ Web3/proto-kit/test-chain/item.ts
@@ -160,5 +160,45 @@ export class Item extends RuntimeModule<{}> {
         );
     }
 
+    @runtimeMethod()
+    public consumeDamageItem(address: PublicKey, id: UInt32) {
+        // Get item
+        const item = this.items.get(new ItemKey({ owner: address, id: id })).value;
+        // Get item's consumable value
+        const isConsumable = item.consumable;
+        // Check if item is consumable or not
+        assert(isConsumable.not(), "this item is not consumable");
+        // Get is item consumed or not
+        const isConsumed= item.consumed;
+        // Check if item is consumed or not
+        assert(isConsumed.not(), "this item is already consumed");
+        // Get current type of the item
+        const type = item.type;
+        // Get current value of the item
+        const value = item.value;
+        // Consume item
+        this.items.set(
+            new ItemKey({ owner: address, id: id }), 
+            new ItemEntity({ 
+                statxp: item.statxp, 
+                damage: item.damage, 
+                defense: item.defense,
+                consumable: item.consumable,
+                upgradable: item.upgradable,
+                consumed: Bool(true),
+                type: item.type,
+                value: item.value,
+            })
+        );
+        // Add consumed item to the consumed items
+        this.consumedItems.set(
+            new ConsumedItemKey({ owner: address, id: id }), 
+            new ConsumedItemEntity({ 
+                type: item.type,
+                value: item.value,
+            })
+        );
+    }
+
     // methods will be added later...
 }
