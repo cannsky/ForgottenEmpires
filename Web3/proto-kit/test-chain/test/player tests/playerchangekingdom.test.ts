c2a29682735551700e8f3f715911b3f7f2ae29cb
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-26 23:45:31 +0300

    add: player change kingdom test

---
+++ Web3/proto-kit/test-chain/test/player tests/playerchangekingdom.test.ts
@@ -0,0 +1,49 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+
+import { PrivateKey, UInt64 } from "o1js";
+
+import { Player } from "../../player";
+
+import { log } from "@proto-kit/common";
+
+log.setLevel("error");
+
+describe("Player", () => {
+    it("should demonstrate how player work", async () => {
+        // Define appchain
+        const appChain = TestingAppChain.fromRuntime({
+            modules: {
+                Player,
+            },
+            config: {
+                Player: {},
+            },
+        });
+        // Start appchain
+        await appChain.Start();
+        // Create a random private key
+        const alicePrivateKey = PrivateKey.random();
+        // Get public key of the private key
+        const alice = alicePrivateKey.toPublicKey();
+        // Set private key
+        appChain.setSigner(alicePrivateKey);
+        // Get Player
+        const player = appChain.runtime.resolve("Player");
+        // Create a tx for testing
+        const tx1 = await appChain.transaction(alice, () => {
+            player.changeKingdom(alice, UInt64.from(1));
+        });
+        // Sign the tx
+        await tx1.sign();
+        // Send the tx
+        await tx1.send();
+        // Produce block
+        const block1 = await appChain.produceBlock();
+        // Get the king changed
+        let aliceKingdom = await appChain.query.runtime.Player.players.get(alice).value.kingdom;
+        // Expect block to be true
+        expect(block1?.txs[0].status).toBe(true);
+        // Expect player kingdom to be 1
+        expect(aliceKingdom?.toBigInt()).toBe(1n);
+    });
+});
