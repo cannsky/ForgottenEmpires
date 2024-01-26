edfeec4fda9d42a416fa0ed3de1e1c890cbb7d8e
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-26 22:25:59 +0300

    add: on chain duel

---
+++ Web3/proto-kit/test-chain/duel.ts
@@ -0,0 +1,38 @@
+// This code is not completed.
+// This code is not audited.
+
+import {
+    RuntimeModule,
+    runtimeModule,
+    state,
+    runtimeMethod
+} from "@proto-kit/module";
+
+import {
+    StateMap,
+    assert
+} from "@proto-kit/protocol";
+
+import {
+    PublicKey,
+    Struct,
+    UInt32,
+    Bool
+} from "o1js";
+
+export class DuelKey extends Struct({
+    owner: PublicKey,
+    id: UInt32,
+}) {}
+
+export class DuelEntity extends Struct({
+    offensiveplayer: PublicKey,
+    defensiveplayer: PublicKey,
+}) {}
+
+@runtimeModule()
+export class Item extends RuntimeModule<{}> {
+
+    @state() public duels = StateMap.from<DuelKey, DuelEntity>(DuelKey, DuelEntity);
+
+}
