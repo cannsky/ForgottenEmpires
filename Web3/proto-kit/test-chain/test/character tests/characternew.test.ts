70f91644c9f5e840f239470c695347be18c12f99
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-01-29 22:22:53 +0300

    add: new character test

---
+++ Web3/proto-kit/test-chain/test/character tests/characternew.test.ts
@@ -0,0 +1,52 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+
+import { PrivateKey } from "o1js";
+
+import { Character } from "../../character";
+
+import { log } from "@proto-kit/common";
+
+log.setLevel("error");
+
+describe("Character New Character Test", () => {
+    it("Tests character new character functionality", async () => {
+        // Define appchain
+        const appChain = TestingAppChain.fromRuntime({
+            modules: {
+                Character,
+            },
+            config: {
+                Character: {},
+            },
+        });
+        // Start appchain
+        await appChain.Start();
+        // Create a random private key
+        const alicePrivateKey = PrivateKey.random();
+        // Get public key of the private key
+        const alice = alicePrivateKey.toPublicKey();
+        // Set private key
+        appChain.setSigner(alicePrivateKey);
+        // Get Character
+        const character = appChain.runtime.resolve("Character");
+
+        // CREATE A NEW CHARACTER FOR TESTING
+
+        // Create a new character for the key
+        const startTX = await appChain.transaction(alice, () => {
+            character.newCharacter();
+        });
+        // Sign the tx
+        await startTX.sign();
+        // Send the tx
+        await startTX.send();
+        // Produce block
+        const blockStart = await appChain.produceBlock();
+        // Get the level of the new character
+        let startLevel = await appChain.query.runtime.Character.characters.get(alice).value.level;
+        // Expect block to be true
+        expect(blockStart?.txs[0].status).toBe(true);
+        // Expect start level to be 1
+        expect(startLevel?.toBigInt()).toBe(1n);
+    });
+});
