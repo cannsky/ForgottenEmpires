f448ae3de29a6fcd514a01fd2efe7c9b745d3f95
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-02-26 17:17:47 +0300

    add: player stats tests for player

---
+++ Web3/proto-kit/test-chain/test/player tests/playerincreaseleadership.test.ts
@@ -0,0 +1,71 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+
+import { PrivateKey, UInt64 } from "o1js";
+
+import { Player } from "../../player";
+
+import { log } from "@proto-kit/common";
+
+log.setLevel("error");
+
+describe("Player Increase Leadership Test", () => {
+    it("Tests player increase leadership functionality", async () => {
+        // Define appchain
+        const appChain = TestingAppChain.fromRuntime({
+            modules: {
+                Player,
+            },
+            config: {
+                Player: {},
+            },
+        });
+        // Start appchain
+        await appChain.Start();
+        // Create a random private key
+        const alicePrivateKey = PrivateKey.random();
+        // Get public key of the private key
+        const alice = alicePrivateKey.toPublicKey();
+        // Set private key
+        appChain.setSigner(alicePrivateKey);
+        // Get Player
+        const player = appChain.runtime.resolve("Player");
+
+        // CREATE A NEW PLAYER FOR TESTING
+
+        // Create a new player for the key
+        const startTX = await appChain.transaction(alice, () => {
+            player.newPlayer();
+        });
+        // Sign the tx
+        await startTX.sign();
+        // Send the tx
+        await startTX.send();
+        // Produce block
+        const blockStart = await appChain.produceBlock();
+        // Get the level of the new character
+        let startLevel = await appChain.query.runtime.Player.players.get(alice).value.level;
+        // Expect block to be true
+        expect(blockStart?.txs[0].status).toBe(true);
+        // Expect start level to be 1
+        expect(startLevel?.toBigInt()).toBe(1n);
+
+        // TEST PLAYER INCREASE LEADERSHIP
+
+        // Create a tx for increase leadership testing
+        const tx1 = await appChain.transaction(alice, () => {
+            player.increaseLeadership();
+        });
+        // Sign the tx
+        await tx1.sign();
+        // Send the tx
+        await tx1.send();
+        // Produce block
+        const block1 = await appChain.produceBlock();
+        // Get the leadership increased
+        let aliceLeadership = await appChain.query.runtime.Player.players.get(alice).value.leadership;
+        // Expect block to be true
+        expect(block1?.txs[0].status).toBe(true);
+        // Expect player leadership to be 2
+        expect(aliceLeadership?.toBigInt()).toBe(1n);
+    });
+});
