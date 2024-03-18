eb79e9b06a477345e0452f81f61d2e434b23eed2
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-03-18 22:16:53 +0300

    add: upgrade on chain rune level functionality

--- Web3/proto-kit/test-chain/rune.ts
+++ Web3/proto-kit/test-chain/rune.ts
@@ -26,10 +26,10 @@ export class RunePointEntity extends Struct({
 }) {}
 
 export class RuneEntity extends Struct({
-    fire: UInt64,
-    water: UInt64,
-    air: UInt64,
-    earth: UInt64,
+    firelevel: UInt64,
+    waterlevel: UInt64,
+    airlevel: UInt64,
+    earthlevel: UInt64,
     maxrunelevel: UInt64,
 }) {}
 
@@ -48,11 +48,11 @@ export class Guild extends RuntimeModule<{}> {
         this.playerRunes.set(
             this.transaction.sender,
             new RuneEntity({ 
-                fire: UInt64.From(0),
-                water: UInt64.From(0),
-                air: UInt64.from(0),
-                earth: UInt64.From(0),
-                maxrunlevel: UInt64.from(0)
+                firelevel: UInt64.From(0),
+                waterlevel: UInt64.From(0),
+                airlevel: UInt64.from(0),
+                earthlevel: UInt64.From(0),
+                maxrunelevel: UInt64.from(10)
             })
         );
         // Create new rune points for the player
@@ -62,6 +62,214 @@ export class Guild extends RuntimeModule<{}> {
                 x: UInt64.From(0),
                 y: UInt64.From(0),
                 z: UInt64.from(0)
+            })
+        );
+    }
+
+    @runtimeMethod()
+    public upgradeFireRune() {
+        // Ensure the caller has rune stats
+        assert(this.playerRunes.get(this.transaction.sender).isSome, "you don't have rune stats");
+        // Ensure the caller has rune points
+        assert(this.playerRunePoints.get(this.transaction.sender).isSome, "you don't have rune points");
+        // Get player rune points
+        const runePoints = this.playerRunePoints.get(this.transaction.sender).value;
+        // Get x rune points
+        const currentXPoints = runePoints.x;
+        // Get y rune points
+        const currentYPoints = runePoints.y;
+        // Ensure at least 2 x points is available
+        assert(currentXPoints.greaterThanOrEqual(UInt64.from(2)), "you don't have enough x points");
+        // Ensure at least 1 y points is available
+        assert(currentYPoints.greaterThanOrEqual(UInt64.from(1)), "you don't have enough y points");
+        // Get player runes
+        const runes = this.playerRunes.get(this.transaction.sender).value;
+        // Get player fire rune level
+        const currentFireLevel = runes.firelevel;
+        // Get player max rune level
+        const currentMaxRuneLevel = runes.maxrunelevel;
+        // Ensure fire rune level is not maxed yet
+        assert(currentFireLevel.lessThanOrEqual(currentMaxRuneLevel), "you already maxed your rune level");
+        // Increase rune level by 1
+        const newFireLevel = currentFireLevel.add(1);
+        // Decrease x rune points by 2
+        const newXPoints = currentXPoints.sub(2);
+        // Decrease y rune points by 1
+        const newYPoints = currentYPoints.sub(1);
+        // Update player runes for new rune level
+        this.playerRunes.set(
+            this.transaction.sender,
+            new RuneEntity({ 
+                firelevel: newFireLevel,
+                waterlevel: runes.waterlevel,
+                airlevel: runes.airlevel,
+                earthlevel: runes.earthlevel,
+                maxrunelevel: runes.maxrunelevel
+            })
+        );
+        // Update player rune points for new rune points
+        this.playerRunePoints.set(
+            this.transaction.sender,
+            new RuneEntity({ 
+                x: newXPoints,
+                y: newYPoints,
+                z: runePoints.z
+            })
+        );
+    }
+
+    @runtimeMethod()
+    public upgradeWaterRune() {
+        // Ensure the caller has rune stats
+        assert(this.playerRunes.get(this.transaction.sender).isSome, "you don't have rune stats");
+        // Ensure the caller has rune points
+        assert(this.playerRunePoints.get(this.transaction.sender).isSome, "you don't have rune points");
+        // Get player rune points
+        const runePoints = this.playerRunePoints.get(this.transaction.sender).value;
+        // Get x rune points
+        const currentXPoints = runePoints.x;
+        // Get z rune points
+        const currentZPoints = runePoints.z;
+        // Ensure at least 1 x points is available
+        assert(currentXPoints.greaterThanOrEqual(UInt64.from(1)), "you don't have enough x points");
+        // Ensure at least 2 z points is available
+        assert(currentZPoints.greaterThanOrEqual(UInt64.from(2)), "you don't have enough z points");
+        // Get player runes
+        const runes = this.playerRunes.get(this.transaction.sender).value;
+        // Get player water rune level
+        const currentWaterLevel = runes.waterlevel;
+        // Get player max rune level
+        const currentMaxRuneLevel = runes.maxrunelevel;
+        // Ensure water rune level is not maxed yet
+        assert(currentWaterLevel.lessThanOrEqual(currentMaxRuneLevel), "you already maxed your rune level");
+        // Increase rune level by 1
+        const newWaterLevel = currentWaterLevel.add(1);
+        // Decrease x rune points by 1
+        const newXPoints = currentXPoints.sub(1);
+        // Decrease z rune points by 2
+        const newZPoints = currentZPoints.sub(2);
+        // Update player runes for new rune level
+        this.playerRunes.set(
+            this.transaction.sender,
+            new RuneEntity({
+                firelevel: runes.firelevel,
+                waterlevel: newWaterLevel,
+                airlevel: runes.airlevel,
+                earthlevel: runes.earthlevel,
+                maxrunelevel: runes.maxrunelevel
+            })
+        );
+        // Update player rune points for new rune points
+        this.playerRunePoints.set(
+            this.transaction.sender,
+            new RuneEntity({ 
+                x: newXPoints,
+                y: runePoints.y,
+                z: newZPoints
+            })
+        );
+    }
+
+    @runtimeMethod()
+    public upgradeAirRune() {
+        // Ensure the caller has rune stats
+        assert(this.playerRunes.get(this.transaction.sender).isSome, "you don't have rune stats");
+        // Ensure the caller has rune points
+        assert(this.playerRunePoints.get(this.transaction.sender).isSome, "you don't have rune points");
+        // Get player rune points
+        const runePoints = this.playerRunePoints.get(this.transaction.sender).value;
+        // Get x rune points
+        const currentXPoints = runePoints.x;
+        // Get y rune points
+        const currentYPoints = runePoints.y;
+        // Ensure at least 1 x points is available
+        assert(currentXPoints.greaterThanOrEqual(UInt64.from(1)), "you don't have enough x points");
+        // Ensure at least 2 y points is available
+        assert(currentYPoints.greaterThanOrEqual(UInt64.from(2)), "you don't have enough y points");
+        // Get player runes
+        const runes = this.playerRunes.get(this.transaction.sender).value;
+        // Get player air rune level
+        const currentAirLevel = runes.airlevel;
+        // Get player max rune level
+        const currentMaxRuneLevel = runes.maxrunelevel;
+        // Ensure air rune level is not maxed yet
+        assert(currentAirLevel.lessThanOrEqual(currentMaxRuneLevel), "you already maxed your rune level");
+        // Increase rune level by 1
+        const newAirLevel = currentAirLevel.add(1);
+        // Decrease x rune points by 1
+        const newXPoints = currentXPoints.sub(1);
+        // Decrease y rune points by 2
+        const newYPoints = currentYPoints.sub(2);
+        // Update player runes for new rune level
+        this.playerRunes.set(
+            this.transaction.sender,
+            new RuneEntity({ 
+                firelevel: runes.firelevel,
+                waterlevel: runes.waterlevel,
+                airlevel: newAirLevel,
+                earthlevel: runes.earthlevel,
+                maxrunelevel: runes.maxrunelevel
+            })
+        );
+        // Update player rune points for new rune points
+        this.playerRunePoints.set(
+            this.transaction.sender,
+            new RuneEntity({ 
+                x: newXPoints,
+                y: newYPoints,
+                z: runePoints.z
+            })
+        );
+    }
+
+    @runtimeMethod()
+    public upgradeEarthRune() {
+        // Ensure the caller has rune stats
+        assert(this.playerRunes.get(this.transaction.sender).isSome, "you don't have rune stats");
+        // Ensure the caller has rune points
+        assert(this.playerRunePoints.get(this.transaction.sender).isSome, "you don't have rune points");
+        // Get player rune points
+        const runePoints = this.playerRunePoints.get(this.transaction.sender).value;
+        // Get x rune points
+        const currentXPoints = runePoints.x;
+        // Get z rune points
+        const currentZPoints = runePoints.z;
+        // Ensure at least 2 x points is available
+        assert(currentXPoints.greaterThanOrEqual(UInt64.from(2)), "you don't have enough x points");
+        // Ensure at least 1 z points is available
+        assert(currentZPoints.greaterThanOrEqual(UInt64.from(1)), "you don't have enough z points");
+        // Get player runes
+        const runes = this.playerRunes.get(this.transaction.sender).value;
+        // Get player earth rune level
+        const currentEarthLevel = runes.earthlevel;
+        // Get player max rune level
+        const currentMaxRuneLevel = runes.maxrunelevel;
+        // Ensure earth rune level is not maxed yet
+        assert(currentEarthLevel.lessThanOrEqual(currentMaxRuneLevel), "you already maxed your rune level");
+        // Increase rune level by 1
+        const newWaterLevel = currentEarthLevel.add(1);
+        // Decrease x rune points by 2
+        const newXPoints = currentXPoints.sub(2);
+        // Decrease z rune points by 1
+        const newZPoints = currentZPoints.sub(1);
+        // Update player runes for new rune level
+        this.playerRunes.set(
+            this.transaction.sender,
+            new RuneEntity({
+                firelevel: runes.firelevel,
+                waterlevel: newWaterLevel,
+                airlevel: runes.airlevel,
+                earthlevel: runes.earthlevel,
+                maxrunelevel: runes.maxrunelevel
+            })
+        );
+        // Update player rune points for new rune points
+        this.playerRunePoints.set(
+            this.transaction.sender,
+            new RuneEntity({ 
+                x: newXPoints,
+                y: runePoints.y,
+                z: newZPoints
             })
         );
     }
