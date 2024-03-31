509566505254a79ffd4905473af4e7fdf9e080cb
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-03-31 17:34:35 +0300

    add: client notify transaction

--- Web3/client/wallet.tsx
+++ Web3/client/wallet.tsx
@@ -5,6 +5,11 @@ import { WalletState } from "./interface";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { PendingTransaction, UnsignedTransaction } from "@proto-kit/sequencer";
+import { useChainStore } from "./chain";
+import { useClientStore } from "./client";
+import { useCallback, useEffect, useMemo } from "react";
+import { MethodIdResolver } from "@proto-kit/module"
+import truncateMiddle from "truncate-middle";
 
 export const useWalletStore = create<WalletState, [["zustand/immer", never]]>(
     immer((set) => ({
@@ -50,3 +55,48 @@ export const useWalletStore = create<WalletState, [["zustand/immer", never]]>(
         }
     }))
 );
+
+export const useNotifyTransactions = () => {
+    const wallet = useWalletStore();
+    const chain = useChainStore();
+    const client = useClientStore();
+
+    const previousPendingTransactions = usePrevious(wallet.pendingTransactions);
+
+    const newPendingTransactions = useMemo(() => {
+        return wallet.pendingTransactions.filter(
+            (pendingTransaction) => !(previousPendingTransactions ?? []).includes(pendingTransaction)
+        );
+    }, [wallet.pendingTransactions, previousPendingTransactions]);
+
+    const notifyTransaction = useCallback(
+    (
+        status: "PENDING" | "SUCCESS" | "FAILURE",
+        transaction: UnsignedTransaction, PendingTransaction
+    ) => {
+        if(!client.client) return;
+
+        const methodIdResolver = client.client.resolveOrFail("MethodIdResolver", MethodIdResolver);
+
+        const resolvedMethodDetails = methodIdResolver.getMethodNameFromId(transaction.methodId.toBigInt());
+
+        if (!resolvedMethodDetails) throw new Error("Unable to resolve method details");
+
+        const [moduleName, methodName] = resolvedMethodDetails;
+
+        const hash = truncateMiddle(transaction.hash().toString(), 15, 15, "...");
+
+        function title() {
+            switch (status) {
+                case "PENDING":
+                    return `Transaction sent: ${moduleName}, ${methodName}`;
+                case "SUCCESS":
+                    return `Transaction successful: ${moduleName}, ${methodName}`;
+                case "FAILURE":
+                    return `Transaction failed: ${moduleName}, ${methodName}`;
+            }
+        }
+
+        console.log(title());
+    }, [client.client]);
+}
