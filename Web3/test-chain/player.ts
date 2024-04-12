b74f7d25b84721ee402d01b53ed4bcf043ebe798
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-04-13 01:59:02 +0300

    update: player proto-kit syntax to latest version

--- Web3/test-chain/player.ts
+++ Web3/test-chain/player.ts
@@ -14,9 +14,12 @@ import {
 } from "@proto-kit/protocol";
 
 import {
-    PublicKey,
-    Struct,
     UInt64
+} from "@proto-kit/library";
+
+import {
+    PublicKey,
+    Struct
 } from "o1js";
 
 export class PlayerEntity extends Struct({
@@ -42,10 +45,10 @@ export class Player extends RuntimeModule<{}> {
     @runtimeMethod()
     public newPlayer() {
         // Check if there is a player or not
-        assert(this.players.get(this.transaction.sender).isSome.not(), "you cannot create two players");
+        assert(this.players.get(this.transaction.sender.value).isSome.not(), "you cannot create two players");
         // Create new player
         this.players.set(
-            this.transaction.sender,
+            this.transaction.sender.value,
             new PlayerEntity({
                 level: UInt64.from(1),
                 xp: UInt64.from(1000)
@@ -53,7 +56,7 @@ export class Player extends RuntimeModule<{}> {
         );
         // Set new player stats
         this.playerStats.set(
-            this.transaction.sender,
+            this.transaction.sender.value,
             new PlayerStatsEntity({
                 charisma: UInt64.from(1),
                 reputation: UInt64.from(1),
@@ -67,9 +70,9 @@ export class Player extends RuntimeModule<{}> {
     @runtimeMethod()
     public levelUp() {
         // Check if there is a player or not
-        assert(this.players.get(this.transaction.sender).isSome, "there is no player on this address");
+        assert(this.players.get(this.transaction.sender.value).isSome, "there is no player on this address");
         // Get player
-        const player = this.players.get(this.transaction.sender).value;
+        const player = this.players.get(this.transaction.sender.value).value;
         // Get current xp value of the player
         const currentXP = player.xp;
         // Get current level value of the player
@@ -77,28 +80,28 @@ export class Player extends RuntimeModule<{}> {
         // Check if the xp is enough for a level up
         assert(currentXP.greaterThanOrEqual(UInt64.from(1000)), "not enough xp");
         // Calculate new level of the player
-        const newLevel = currentLevel.add(1);
+        const newLevel = UInt64.from(currentLevel).add(UInt64.from(1));
         // calculate new xp of the player
-        const newXP = currentXP.sub(1000);
+        const newXP = UInt64.from(currentXP).sub(UInt64.from(1000));
         // Set new xp and level of the player
         this.players.set(
-            this.transaction.sender, 
+            this.transaction.sender.value, 
             new PlayerEntity({ 
                 level: newLevel, 
                 xp: newXP
             })
         );
         // Check if there is a player stats or not
-        assert(this.playerStats.get(this.transaction.sender).isSome, "there is no player stats on this address");
+        assert(this.playerStats.get(this.transaction.sender.value).isSome, "there is no player stats on this address");
         // Get player stats
         const playerStats = this.playerStats.get(this.transaction.sender).value;
         // Get current charisma
         const currentCharisma = playerStats.charisma;
         // Increase current charisma by 1
-        const newCharisma = currentCharisma.add(1);
+        const newCharisma = UInt64.from(currentCharisma).add(UInt64.from(1));
         // Increase charisma by 1 on each level up
         this.playerStats.set(
-            this.transaction.sender,
+            this.transaction.sender.value,
             new PlayerStatsEntity({
                 charisma: newCharisma,
                 reputation: playerStats.reputation,
@@ -112,11 +115,11 @@ export class Player extends RuntimeModule<{}> {
     @runtimeMethod()
     public increaseLeadership() {
         // Check if there is a player or not
-        assert(this.players.get(this.transaction.sender).isSome, "there is no player on this address");
+        assert(this.players.get(this.transaction.sender.value).isSome, "there is no player on this address");
         // Check if there is a player stats or not
-        assert(this.playerStats.get(this.transaction.sender).isSome, "there is no player stats on this address");
+        assert(this.playerStats.get(this.transaction.sender.value).isSome, "there is no player stats on this address");
         // Get player stats
-        const playerStats = this.playerStats.get(this.transaction.sender).value;
+        const playerStats = this.playerStats.get(this.transaction.sender.value).value;
         // Get current charisma of the player
         const currentCharisma = playerStats.charisma;
         // Get current leadership of the player
@@ -124,12 +127,12 @@ export class Player extends RuntimeModule<{}> {
         // Check if the charisma is enough for a increasing the leadership
         assert(currentCharisma.greaterThanOrEqual(UInt64.from(1)), "not enough charisma");
         // Calculate new leadership of the player
-        const newLeadership = currentLeadership.add(1);
+        const newLeadership = UInt64.from(currentLeadership).add(UInt64.from(1));
         // calculate new charisma of the player
-        const newCharisma = currentCharisma.sub(1);
+        const newCharisma = UInt64.from(currentCharisma).sub(UInt64.from(1));
         // Set new charisma and leadership of the player
         this.playerStats.set(
-            this.transaction.sender,
+            this.transaction.sender.value,
             new PlayerStatsEntity({
                 charisma: newCharisma,
                 reputation: playerStats.reputation,
@@ -143,11 +146,11 @@ export class Player extends RuntimeModule<{}> {
     @runtimeMethod()
     public increaseBravery() {
         // Check if there is a player or not
-        assert(this.players.get(this.transaction.sender).isSome, "there is no player on this address");
+        assert(this.players.get(this.transaction.sender.value).isSome, "there is no player on this address");
         // Check if there is a player stats or not
-        assert(this.playerStats.get(this.transaction.sender).isSome, "there is no player stats on this address");
+        assert(this.playerStats.get(this.transaction.sender.value).isSome, "there is no player stats on this address");
         // Get player stats
-        const playerStats = this.playerStats.get(this.transaction.sender).value;
+        const playerStats = this.playerStats.get(this.transaction.sender.value).value;
         // Get current reputation of the player
         const currentReputation = playerStats.reputation;
         // Get current bravery of the player
@@ -155,12 +158,12 @@ export class Player extends RuntimeModule<{}> {
         // Check if the reputation is enough for a increasing the bravery
         assert(currentReputation.greaterThanOrEqual(UInt64.from(1)), "not enough bravery");
         // Calculate new bravery of the player
-        const newBravery = currentBravery.add(1);
+        const newBravery = UInt64.from(currentBravery).add(UInt64.from(1));
         // calculate new reputation of the player
-        const newReputation = currentReputation.sub(1);
+        const newReputation = UInt64.from(currentReputation).sub(UInt64.from(1));
         // Set new reputation and bravery of the player
         this.playerStats.set(
-            this.transaction.sender,
+            this.transaction.sender.value,
             new PlayerStatsEntity({
                 charisma: playerStats.charisma,
                 reputation: newReputation,
@@ -174,19 +177,19 @@ export class Player extends RuntimeModule<{}> {
     @runtimeMethod()
     public getTotalLeadership() {
         // Check if there is a player or not
-        assert(this.players.get(this.transaction.sender).isSome, "there is no player on this address");
+        assert(this.players.get(this.transaction.sender.value).isSome, "there is no player on this address");
         // Check if there is a player stats or not
-        assert(this.playerStats.get(this.transaction.sender).isSome, "there is no player stats on this address");
+        assert(this.playerStats.get(this.transaction.sender.value).isSome, "there is no player stats on this address");
         // Get player
-        const player = this.players.get(this.transaction.sender).value;
+        const player = this.players.get(this.transaction.sender.value).value;
         // Get player stats
-        const playerStats = this.playerStats.get(this.transaction.sender).value;
+        const playerStats = this.playerStats.get(this.transaction.sender.value).value;
         // Get player level
         const playerLevel = player.level;
         // Get player leadership
         const playerLeadership = playerStats.leadership;
         // Get total leadership
-        const totalLoadership = playerLevel.mul(playerLeadership);
+        const totalLoadership = UInt64.from(playerLevel).mul(UInt64.from(playerLeadership));
         // Return total leadership calculated
         return totalLoadership;
     }
@@ -194,19 +197,19 @@ export class Player extends RuntimeModule<{}> {
     @runtimeMethod()
     public getTotalBravery() {
         // Check if there is a player or not
-        assert(this.players.get(this.transaction.sender).isSome, "there is no player on this address");
+        assert(this.players.get(this.transaction.sender.value).isSome, "there is no player on this address");
         // Check if there is a player stats or not
-        assert(this.playerStats.get(this.transaction.sender).isSome, "there is no player stats on this address");
+        assert(this.playerStats.get(this.transaction.sender.value).isSome, "there is no player stats on this address");
         // Get player
-        const player = this.players.get(this.transaction.sender).value;
+        const player = this.players.get(this.transaction.sender.value).value;
         // Get player stats
-        const playerStats = this.playerStats.get(this.transaction.sender).value;
+        const playerStats = this.playerStats.get(this.transaction.sender.value).value;
         // Get player bravery
         const playerLevel = player.level;
         // Get player bravery
         const playerBravery = playerStats.bravery;
         // Get total bravery
-        const totalBravery = playerLevel.mul(playerBravery);
+        const totalBravery = UInt64.from(playerLevel).mul(UInt64.from(playerBravery));
         // Return total bravery calculated
         return totalBravery;
     }
