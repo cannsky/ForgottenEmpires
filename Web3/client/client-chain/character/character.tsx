1df9be756a7738a9f2649f1ec3fbb8828c7e421d
Can Gök <44663880+cannsky@users.noreply.github.com>
2024-04-19 02:56:33 +0300

    add: character client interactions

--- Web3/client/client-chain/character/character.tsx
+++ Web3/client/client-chain/character/character.tsx
@@ -9,6 +9,7 @@ import { useEffect, useCallback } from "react";
 import { CharacterState } from "./interface";
 import { PendingTransaction, UnsignedTransaction } from "@proto-kit/sequencer";
 import { useWalletStore } from "../../wallet";
+import { UInt64 } from "@proto-kit/library";
 
 function isPendingTransaction(transaction: PendingTransaction | UnsignedTransaction | undefined) 
     : asserts transaction is PendingTransaction {
@@ -20,7 +21,7 @@ export class CharacterKey extends Struct({
     id: UInt32,
 }) {}
 
-export const usePlayerStore = create<CharacterState, [["zustand/immer", never]]>(
+export const useCharacterStore = create<CharacterState, [["zustand/immer", never]]>(
     immer((set) => ({
         loading: Boolean(false),
         characters: {},
@@ -45,5 +46,144 @@ export const usePlayerStore = create<CharacterState, [["zustand/immer", never]]>
                 state.players[address] = { character: clientCharacter };
             });
         },
+        async newCharacter(client: Client, address: string, characterType: number) {
+            // Get character
+            const character = client.runtime.resolve("Character");
+            // Get public key of sender
+            const sender = PublicKey.fromBase58(address);
+            // Create transaction
+            const tx = await client.transaction(sender, () => {
+                character.newCharacter(
+                    UInt64.from(characterType)
+                );
+            });
+            // Sign transaction 
+            await tx.sign();
+            // Send transaction
+            await tx.send();
+            // Check if the transaction is pending or not
+            isPendingTransaction(tx.transaction);
+            // Return transaction
+            return tx.transaction;
+        },
+        async levelUP(client: Client, address: string, characterId: number) {
+            // Get character
+            const character = client.runtime.resolve("Character");
+            // Get public key of sender
+            const sender = PublicKey.fromBase58(address);
+            // Create transaction
+            const tx = await client.transaction(sender, () => {
+                character.levelUP(
+                    UInt64.from(characterId)
+                );
+            });
+            // Sign transaction 
+            await tx.sign();
+            // Send transaction
+            await tx.send();
+            // Check if the transaction is pending or not
+            isPendingTransaction(tx.transaction);
+            // Return transaction
+            return tx.transaction;
+        },
+        async upgradeDamage(client: Client, address: string, characterId: number) {
+            // Get character
+            const character = client.runtime.resolve("Character");
+            // Get public key of sender
+            const sender = PublicKey.fromBase58(address);
+            // Create transaction
+            const tx = await client.transaction(sender, () => {
+                character.upgradeDamage(
+                    UInt64.from(characterId)
+                );
+            });
+            // Sign transaction 
+            await tx.sign();
+            // Send transaction
+            await tx.send();
+            // Check if the transaction is pending or not
+            isPendingTransaction(tx.transaction);
+            // Return transaction
+            return tx.transaction;
+        },
+        async upgradeDefense(client: Client, address: string, characterId: number) {
+            // Get character
+            const character = client.runtime.resolve("Character");
+            // Get public key of sender
+            const sender = PublicKey.fromBase58(address);
+            // Create transaction
+            const tx = await client.transaction(sender, () => {
+                character.upgradeDefense(
+                    UInt64.from(characterId)
+                );
+            });
+            // Sign transaction 
+            await tx.sign();
+            // Send transaction
+            await tx.send();
+            // Check if the transaction is pending or not
+            isPendingTransaction(tx.transaction);
+            // Return transaction
+            return tx.transaction;
+        },
+        async upgradeMaxUpgrade(client: Client, address: string, characterId: number) {
+            // Get character
+            const character = client.runtime.resolve("Character");
+            // Get public key of sender
+            const sender = PublicKey.fromBase58(address);
+            // Create transaction
+            const tx = await client.transaction(sender, () => {
+                character.upgradeMaxUpgrade(
+                    UInt64.from(characterId)
+                );
+            });
+            // Sign transaction 
+            await tx.sign();
+            // Send transaction
+            await tx.send();
+            // Check if the transaction is pending or not
+            isPendingTransaction(tx.transaction);
+            // Return transaction
+            return tx.transaction;
+        },
+        async changeWorld(client: Client, address: string, characterId: number, worldId: number) {
+            // Get character
+            const character = client.runtime.resolve("Character");
+            // Get public key of sender
+            const sender = PublicKey.fromBase58(address);
+            // Create transaction
+            const tx = await client.transaction(sender, () => {
+                character.upgradeMaxUpgrade(
+                    UInt64.from(characterId), 
+                    UInt64.from(worldId)
+                );
+            });
+            // Sign transaction 
+            await tx.sign();
+            // Send transaction
+            await tx.send();
+            // Check if the transaction is pending or not
+            isPendingTransaction(tx.transaction);
+            // Return transaction
+            return tx.transaction;
+        },
     }))
 );
+
+export const useGetSelectedCharacter = (characterId: number) => {
+    // Get client
+    const client = useClientStore();
+    // Get character
+    const character = useCharacterStore();
+    // Get wallet
+    const wallet = useWalletStore();
+
+    return useCallback(async() => {
+        // If client or wallet is not defined return
+        if(!client.client || !wallet.wallet) return;
+        // New pending transaction
+        const pendingTransaction = await character.getSelectedCharacter(client.client, wallet.wallet, characterId);
+        // Add pending transaction to wallet
+        wallet.addPendingTransaction(pendingTransaction);
+    }, [client.client, wallet.wallet]);
+};
